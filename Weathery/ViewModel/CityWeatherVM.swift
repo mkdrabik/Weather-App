2a7054f0a312d6aee3b002d8352207b0a41b8d29
Mason Drabik <111398844+mkdrabik@users.noreply.github.com>
2024-01-13 15:34:43 -0500

    Update CityWeatherVM.swift

--- Weathery/ViewModel/CityWeatherVM.swift
+++ Weathery/ViewModel/CityWeatherVM.swift
@@ -8,18 +8,16 @@
 import Foundation
 
 
-/// A wrapper struct that will contain all the logic for presenting our Weather data.
-/// It accepts a `Weather` struct, and contains methods for representing the data contained in that struct
+// A wrapper struct that will contain all the logic for presenting our Weather data.
+// It accepts a `Weather` struct, and contains methods for representing the data contained in that struct
 struct CityWeatherVM {
     
     /// The weather data being processed by this view model
     let model: Weather
     let unit: String
+
     
-    
-    // MARK: Here's where you should add logic for different representations of the temperature in Fahrenheit, Celsius, etc. You'll also want to make the methods for representing the other data (e.g., the weather icon)
-    
-    /// A getter that returns the formatted temperature, or "No temp" if no data is available. It's currently in Kelvin.
+    // A getter that returns the formatted temperature, or "No temp" if no data is available. 
     var formattedCurrentTemp: String {
         guard let temp = model.main?.temp else { return "No temp" }
         let temperature = Measurement(value: temp, unit: UnitTemperature.kelvin)
